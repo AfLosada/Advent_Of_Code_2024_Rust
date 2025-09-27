b88e9a1ad4711d33ef462466d9dd5da88d6b6fcf
Andres Felipe Losada <nedrocoli@gmail.com>
2025-09-27 15:12:07 -0500

    wip day5

--- src/day5/day5.rs
+++ src/day5/day5.rs
@@ -33,6 +33,7 @@ impl Page {
 
 struct RuleTrie {
     rule_trie: Vec<TrieNode>,
+    rule_map_trie: HashMap<(String, String), TrieNode>,
 }
 
 impl RuleTrie {
@@ -47,7 +48,26 @@ impl RuleTrie {
                     map.push(trie_node);
                     map
                 });
-        RuleTrie { rule_trie }
+        let rule_map_trie = page
+            .raw_rules
+            .iter()
+            .fold(HashMap::new(), |mut map, rule: &String| {
+                let split_rules = rule.split("|").collect::<Vec<&str>>();
+                let first = split_rules.first().unwrap();
+                let second = split_rules.get(1).unwrap();
+                map.insert(
+                    (first.to_owned().to_owned(), second.to_owned().to_owned()),
+                    TrieNode::new(rule),
+                );
+                map
+            });
+        RuleTrie {
+            rule_trie,
+            rule_map_trie,
+        }
+    }
+    fn find_rules_map(&self, combination: (String, String)) -> Option<&TrieNode> {
+        self.rule_map_trie.get(&combination)
     }
     fn find_rules(&self, rule: String) -> Option<String> {
         let is_equal = |new_rule: &str| &rule == new_rule;
@@ -56,12 +76,10 @@ impl RuleTrie {
             match &node.value {
                 Some(v) => is_equal(&v),
                 None => {
-                  //TODO: FIX only get rules that match
+                    //TODO: FIX only get rules that match
                     let rule_option = node.nodes.get(&rule);
                     match rule_option {
-                        Some(_) => {
-                          true
-                        },
+                        Some(_) => true,
                         None => false,
                     }
                 }
@@ -72,6 +90,35 @@ impl RuleTrie {
             None => None,
         }
     }
+    fn find_rule_from_pair(&self, (first_rule, second_rule): (String, String)) -> Option<String> {
+        self.rule_trie
+            .iter()
+            .find_map(|node| match node.nodes.get(&first_rule) {
+                Some(first_trie) => {
+                    // first_trie.print(0, 3);
+                    match first_trie.nodes.get(&second_rule) {
+                        Some(second_trie) => second_trie.value.clone(),
+                        None => None,
+                    }
+                }
+                None => None,
+            })
+    }
+}
+
+fn create_combinations<T: Clone>(mut acc: Vec<(T, T)>, vec: Vec<T>, size: u8) -> Vec<(T, T)> {
+    if vec.len() == 1 {
+        return acc;
+    }
+    let first = vec.first().unwrap();
+    let remainder = &vec[1..];
+    let mut combinations = remainder
+        .iter()
+        .map(|curr| (first.clone(), curr.clone()))
+        .collect::<Vec<_>>();
+    acc.append(&mut combinations);
+    //TODO: fix combinations
+    create_combinations(acc, remainder.to_vec(), size)
 }
 
 #[derive(Clone)]
@@ -85,31 +132,41 @@ impl TrieNode {
         cyclical_tree_build(rule, rule.split("|").collect())
     }
     fn print(&self, level: u8, depth: u8) {
-      if level > depth {
-        return
-      }
-      println!("-----------------LEVEL: {}--------------------------------------------", level);
-      println!("value: {}", self.value.clone().unwrap_or("None".to_owned()));
-      println!("nodes: {:?}", self.nodes.clone().into_keys());
-      self.nodes.clone().into_iter().for_each(|(key, node)|{
-        println!("-----------------KEY: {}--------------------------------------------", key);
-        node.print(level+1, depth);
-      });
+        if level > depth {
+            return;
+        }
+        println!(
+            "-----------------LEVEL: {}--------------------------------------------",
+            level
+        );
+        println!("value: {}", self.value.clone().unwrap_or("None".to_owned()));
+        println!("nodes: {:?}", self.nodes.clone().into_keys());
+        self.nodes.clone().into_iter().for_each(|(key, node)| {
+            println!(
+                "-----------------KEY: {}--------------------------------------------",
+                key
+            );
+            node.print(level + 1, depth);
+        });
     }
 
     fn get_value_list(&self) -> Vec<String> {
-      match &self.value {
-        Some(v) => vec![v.to_string()],
-        None => self.nodes.iter().map(|(key, node)|{
-          node.get_value_list()
-        }).flatten().collect::<Vec<_>>()
-      }
+        match &self.value {
+            Some(v) => vec![v.to_string()],
+            None => self
+                .nodes
+                .iter()
+                .map(|(key, node)| node.get_value_list())
+                .flatten()
+                .collect::<Vec<_>>(),
+        }
     }
 }
 
 fn cyclical_tree_build(original_rule: &str, remaining_words: Vec<&str>) -> TrieNode {
     let mut nodes: HashMap<String, TrieNode> = HashMap::new();
     let first_letter = remaining_words.first().cloned();
+    let second_letter = remaining_words.first().cloned();
     let a = if remaining_words.len() > 0 {
         remaining_words[1..].to_owned()
     } else {
@@ -117,7 +174,7 @@ fn cyclical_tree_build(original_rule: &str, remaining_words: Vec<&str>) -> TrieN
     };
     let remaining_letters = a;
 
-    match first_letter {
+    let mut curr_trie = match first_letter {
         Some(first_letter) => {
             let node = nodes.get(first_letter).cloned();
             match node {
@@ -125,7 +182,7 @@ fn cyclical_tree_build(original_rule: &str, remaining_words: Vec<&str>) -> TrieN
                 None => {
                     nodes.insert(
                         first_letter.to_string(),
-                        cyclical_tree_build(original_rule, remaining_letters),
+                        cyclical_tree_build(original_rule, remaining_letters.clone()),
                     );
                     TrieNode { nodes, value: None }
                 }
@@ -135,7 +192,30 @@ fn cyclical_tree_build(original_rule: &str, remaining_words: Vec<&str>) -> TrieN
             nodes: HashMap::new(),
             value: Some(original_rule.to_string()),
         },
-    }
+    };
+    match second_letter {
+        Some(second_letter) => {
+            let node = curr_trie.nodes.get(second_letter).cloned();
+            match node {
+                Some(n) => n,
+                None => {
+                    curr_trie.nodes.insert(
+                        second_letter.to_string(),
+                        cyclical_tree_build(original_rule, remaining_letters),
+                    );
+                    TrieNode {
+                        nodes: curr_trie.nodes.clone(),
+                        value: None,
+                    }
+                }
+            }
+        }
+        None => TrieNode {
+            nodes: HashMap::new(),
+            value: Some(original_rule.to_string()),
+        },
+    };
+    curr_trie
 }
 
 fn is_rule_valid_on_line(line: &str, rule: &str) -> Option<bool> {
@@ -188,15 +268,26 @@ mod tests {
             .raw_lines
             .iter()
             .filter(|line| {
-                println!("checking line: ");
-                let mut rules: Vec<Option<String>> = line
-                    .split(",")
-                    .map(|letter| rule_trie.find_rules(letter.to_string()))
-                    .collect();
+                println!("checking line: {}", line);
+                let combinations =
+                    create_combinations(vec![], line.split(",").collect::<Vec<&str>>(), 2);
+                println!("combinations: {:?}", combinations);
+                let mut rules = combinations
+                    .into_iter()
+                    .map(|pair| {
+                        //rule_trie.find_rule_from_pair((pair.0.to_owned(), pair.1.to_owned()))
+                        rule_trie.find_rules_map((pair.0.to_owned(), pair.1.to_owned()))
+                    })
+                    .collect::<Vec<_>>();
                 rules.retain(|rule| rule.is_some());
-                println!("rules that match: {:?}", rules);
+                println!("rules that match: {:?}", rules.len());
+
+                if rules.iter().len() == 0 {
+                    return false;
+                }
                 rules.iter().all(|rule| {
-                    let rule = rule.clone().unwrap();
+                    let binding = rule.unwrap().get_value_list();
+                    let rule = binding.first().unwrap();
                     println!("match line {} with rule {}", line, &rule);
                     match is_rule_valid_on_line(line, rule.as_str()) {
                         Some(true) => true,
