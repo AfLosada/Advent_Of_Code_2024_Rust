8feaeea8e3728f3c24afb2a719b4b9ef1c483f01
Andres Felipe Losada <nedrocoli@gmail.com>
2025-10-03 16:55:22 -0500

    almost ready for day 2

--- src/day5/day5.rs
+++ src/day5/day5.rs
@@ -16,7 +16,7 @@ fn extract_rules_and_rows_from_input(input: &str) -> Page {
         return current_page;
     })
 }
-
+#[derive(Clone)]
 struct Page {
     raw_rules: Vec<String>,
     raw_lines: Vec<String>,
@@ -34,6 +34,7 @@ impl Page {
 struct RuleTrie {
     rule_trie: Vec<TrieNode>,
     rule_map_trie: HashMap<(String, String), TrieNode>,
+    page: Page,
 }
 
 impl RuleTrie {
@@ -64,6 +65,7 @@ impl RuleTrie {
         RuleTrie {
             rule_trie,
             rule_map_trie,
+            page: page.clone(),
         }
     }
     fn find_rules_map(&self, combination: (String, String)) -> Option<&TrieNode> {
@@ -104,6 +106,97 @@ impl RuleTrie {
                 None => None,
             })
     }
+
+    fn get_lines_that_comply_rules(&self) -> Vec<(String, Vec<Option<&TrieNode>>)> {
+        let lines_that_comply_rules = self
+            .page
+            .raw_lines
+            .iter()
+            .filter_map(|line| {
+                println!("checking line: {}", line);
+                let combinations =
+                    create_combinations(vec![], line.split(",").collect::<Vec<&str>>(), 2);
+                println!("combinations: {:?}", combinations);
+                let mut rules = combinations
+                    .into_iter()
+                    .map(|pair| {
+                        //rule_trie.find_rule_from_pair((pair.0.to_owned(), pair.1.to_owned()))
+                        self.find_rules_map((pair.0.to_owned(), pair.1.to_owned()))
+                    })
+                    .collect::<Vec<_>>();
+                rules.retain(|rule| rule.is_some());
+                println!("rules that match: {:?}", rules.len());
+
+                if rules.iter().len() == 0 {
+                    return None;
+                }
+                let comply = match {
+                    rules.iter().all(|rule| {
+                        let binding = rule.unwrap().get_value_list();
+                        let rule = binding.first().unwrap();
+                        println!("match line {} with rule {}", line, &rule);
+                        match is_rule_valid_on_line(line, rule.as_str()) {
+                            Some(true) => true,
+                            _ => false,
+                        }
+                    })
+                } {
+                    true => Some((line.to_string(), rules.to_owned())),
+                    false => None,
+                };
+                comply
+            })
+            .collect::<Vec<_>>();
+        lines_that_comply_rules.iter().for_each(|line| {
+            println!("line that complies: {}", line.0.as_str());
+        });
+        lines_that_comply_rules
+    }
+    fn get_lines_that_dont_comply_rules(&self) -> Vec<(String, Vec<Option<&TrieNode>>)> {
+        let lines_that_comply_rules = self
+            .page
+            .raw_lines
+            .iter()
+            .filter_map(|line| {
+                println!("checking line: {}", line);
+                let combinations =
+                    create_combinations(vec![], line.split(",").collect::<Vec<&str>>(), 2);
+                println!("combinations: {:?}", combinations);
+                let mut rules = combinations
+                    .into_iter()
+                    .map(|pair| {
+                        //rule_trie.find_rule_from_pair((pair.0.to_owned(), pair.1.to_owned()))
+                        self.find_rules_map((pair.0.to_owned(), pair.1.to_owned()))
+                    })
+                    .collect::<Vec<_>>();
+                rules.retain(|rule| rule.is_some());
+                println!("rules that match: {:?}", rules.len());
+
+                if rules.iter().len() == 0 {
+                    return None;
+                }
+                let comply = match {
+                    rules.iter().any(|rule| {
+                        let binding = rule.unwrap().get_value_list();
+                        let rule = binding.first().unwrap();
+                        println!("match line {} with rule {}", line, &rule);
+                        !match is_rule_valid_on_line(line, rule.as_str()) {
+                            Some(true) => true,
+                            _ => false,
+                        }
+                    })
+                } {
+                    true => Some((line.to_string(), rules.to_owned())),
+                    false => None,
+                };
+                comply
+            })
+            .collect::<Vec<_>>();
+        lines_that_comply_rules.iter().for_each(|line| {
+            println!("line that complies: {}", line.0.as_str());
+        });
+        lines_that_comply_rules
+    }
 }
 
 fn create_combinations<T: Clone>(mut acc: Vec<(T, T)>, vec: Vec<T>, size: u8) -> Vec<(T, T)> {
@@ -114,10 +207,9 @@ fn create_combinations<T: Clone>(mut acc: Vec<(T, T)>, vec: Vec<T>, size: u8) ->
     let remainder = &vec[1..];
     let mut combinations = remainder
         .iter()
-        .map(|curr| (first.clone(), curr.clone()))
+        .flat_map(|curr| vec![(first.clone(), curr.clone()), (curr.clone(), first.clone())])
         .collect::<Vec<_>>();
     acc.append(&mut combinations);
-    //TODO: fix combinations
     create_combinations(acc, remainder.to_vec(), size)
 }
 
@@ -251,8 +343,72 @@ fn get_valid_rule(node: &TrieNode, word_list: &[String]) -> Option<String> {
     }
 }
 
+fn get_midpoints_of_lines(lines: &Vec<String>) -> Vec<i32> {
+    lines
+        .into_iter()
+        .map(|curr: &String| {
+            let split_lines = curr.split(",").collect::<Vec<_>>();
+            split_lines[split_lines.len() / 2].parse::<i32>().unwrap()
+        })
+        .collect::<Vec<_>>()
+}
+
+fn fix_line(line: &Vec<String>, rules: &Vec<String>) -> String {
+    println!("line to fix: {}", line.join(","));
+    let invalid_rules = rules
+        .iter()
+        .filter(|rule| {
+            let split_line = &line.join(",");
+            !is_rule_valid_on_line(split_line, rule.as_str()).is_some()
+        })
+        .collect::<Vec<_>>();
+    if invalid_rules.len() == 0 {
+        return line.join(",");
+    }
+    let rule_to_fix = rules[0].clone();
+    println!("rule to fix: {}", rule_to_fix);
+    let fixed_line_after_rule = fix_rule(&line, &rule_to_fix)
+        .split(",")
+        .map(|l| l.to_string())
+        .collect::<Vec<_>>();
+    println!("old line: {}", line.join(","));
+    println!("fixed line: {}", fixed_line_after_rule.join(","));
+    fix_line(&fixed_line_after_rule, rules)
+}
+
+fn fix_rule(line: &Vec<String>, rule: &String) -> String {
+    let split_rules = rule.split("|").collect::<Vec<_>>();
+    let first_number_of_rule = split_rules[0];
+    let second_number_of_rule = split_rules[1];
+    let first_number_position_in_vec = line.iter().position(|n| n == first_number_of_rule).unwrap();
+    let second_number_position_in_vec = line
+        .iter()
+        .position(|n| n == second_number_of_rule)
+        .unwrap();
+    let new_string = move_item_in_vec(
+        line,
+        second_number_position_in_vec,
+        first_number_position_in_vec + 1,
+    );
+    new_string.join(",")
+}
+
+fn move_item_in_vec<T: Clone>(vec: &Vec<T>, origin: usize, destination: usize) -> Vec<T> {
+    let item = vec[origin].clone();
+    let slice_without_item = [&vec[..origin], &vec[origin + 1..]].concat();
+    [
+        &slice_without_item[..destination],
+        &[item],
+        &slice_without_item[destination..],
+    ]
+    .concat()
+}
+
 #[cfg(test)]
 mod tests {
+    use core::fmt;
+    use std::string;
+
     use super::*;
     #[test]
     fn test_read_input() {
@@ -264,40 +420,59 @@ mod tests {
     fn test_build_rules() {
         let page = extract_rules_and_rows_from_input("test.txt");
         let rule_trie = RuleTrie::new(&page);
-        let lines_that_comply_rules = page
-            .raw_lines
+        let lines = rule_trie
+            .get_lines_that_comply_rules()
             .iter()
-            .filter(|line| {
-                println!("checking line: {}", line);
-                let combinations =
-                    create_combinations(vec![], line.split(",").collect::<Vec<&str>>(), 2);
-                println!("combinations: {:?}", combinations);
-                let mut rules = combinations
-                    .into_iter()
-                    .map(|pair| {
-                        //rule_trie.find_rule_from_pair((pair.0.to_owned(), pair.1.to_owned()))
-                        rule_trie.find_rules_map((pair.0.to_owned(), pair.1.to_owned()))
-                    })
-                    .collect::<Vec<_>>();
-                rules.retain(|rule| rule.is_some());
-                println!("rules that match: {:?}", rules.len());
-
-                if rules.iter().len() == 0 {
-                    return false;
-                }
-                rules.iter().all(|rule| {
-                    let binding = rule.unwrap().get_value_list();
-                    let rule = binding.first().unwrap();
-                    println!("match line {} with rule {}", line, &rule);
-                    match is_rule_valid_on_line(line, rule.as_str()) {
-                        Some(true) => true,
-                        _ => false,
-                    }
+            .map(|line| line.0.clone())
+            .collect::<Vec<String>>();
+        let sum: i32 = get_midpoints_of_lines(&lines).into_iter().sum();
+        println!("Sum of midpoints: {}", sum);
+        assert_eq!(sum, 143);
+    }
+    #[test]
+    fn test_build_rules_2() {
+        let page = extract_rules_and_rows_from_input("test.txt");
+        let rule_trie = RuleTrie::new(&page);
+        let lines = rule_trie.get_lines_that_dont_comply_rules();
+        println!(
+            "lines to fix: {:?}",
+            lines
+                .iter()
+                .map(|l| {
+                    let aas = l.0.clone();
+                    aas
                 })
+                .collect::<Vec<_>>()
+        );
+        let fixed_lines = lines
+            .iter()
+            .map(|(line, rules)| {
+                let split_line = line.split(",").map(|l| l.to_string()).collect::<Vec<_>>();
+                let rules = rules
+                    .iter()
+                    .map(|rule| rule.unwrap().get_value_list().first().unwrap().to_string())
+                    .collect::<Vec<_>>();
+                println!("rules to fix: {}", rules.join(","));
+                println!("line to fix: {}", line);
+                fix_line(&split_line, &rules)
             })
             .collect::<Vec<_>>();
-        lines_that_comply_rules.iter().for_each(|line| {
-            println!("line that complies: {}", line.as_str());
-        });
+        println!("{:#}", fixed_lines.join("\n"));
+        let sum: i32 = get_midpoints_of_lines(&fixed_lines).into_iter().sum();
+        println!("Sum of midpoints: {}", sum);
+        assert_eq!(sum, 123);
+    }
+    #[test]
+    fn test_answer() {
+        let page = extract_rules_and_rows_from_input("input.txt");
+        let rule_trie = RuleTrie::new(&page);
+        let lines = rule_trie
+            .get_lines_that_comply_rules()
+            .iter()
+            .map(|line| line.0.clone())
+            .collect::<Vec<String>>();
+        let sum: i32 = get_midpoints_of_lines(&lines).into_iter().sum();
+        println!("Sum of midpoints: {}", sum);
+        assert_eq!(sum, 4609);
     }
 }
